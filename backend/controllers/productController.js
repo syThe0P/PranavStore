49d52b5fc934143241e6256829345d2670d5f55d
Pranav Kumar <pranavkumar1522@gmail.com>
2024-05-17 21:01:33 +0530

    Some api features and controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ import Product from "../models/ProductModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiFeatures } from "../utils/apiFeatures.js";
 
 //create product - admin
 const createProduct = asyncHandler(async(req, res) =>{
@@ -15,10 +16,14 @@ const createProduct = asyncHandler(async(req, res) =>{
 
 //get all products
 const getAllProducts = asyncHandler(async(req, res) =>{
-    const products = await Product.find({});
+    const resultPerPage = 5;
+    const productCount = await Product.countDocument();
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
+    const products = await apiFeature.query;
     res.json(new ApiResponse(
         201,
         products,
+        productCount,
         "All products displayed"
     ));
 
@@ -62,4 +67,18 @@ const deleteProduct = asyncHandler(async(req, res) => {
 
 })
 
-export {createProduct,getAllProducts,updateProduct,deleteProduct};
+//Get single product
+const getProductDetails = asyncHandler(async(req,res) =>{
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        throw new ApiError(401, "Product not found");
+    }
+    res.json(new ApiResponse(
+        201,
+        product,
+        "Product Found"
+    ))
+
+})
+
+export {createProduct,getAllProducts,updateProduct,deleteProduct, getProductDetails};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getAllProducts, getProductDetails, updateProduct } from "../controllers/productController.js";
 const router = express.Router();
 
 router.route('/product/new').post(createProduct);
 router.route('/products').get(getAllProducts);
-router.route('/product/:id').put(updateProduct).delete(deleteProduct)
+router.route('/product/:id').put(updateProduct).delete(deleteProduct).get(getProductDetails)
 
 
 export default router;
