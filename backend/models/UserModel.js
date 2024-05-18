993129a6c9471347aecceeccd57d1008ef77bd54
Pranav Kumar <pranavkumar1522@gmail.com>
2024-05-19 02:27:42 +0530

    edit env file and create some functions and usermodel

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import validator from "validator";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
 
 const userSchema = mongoose.Schema(
   {
@@ -15,14 +18,67 @@ const userSchema = mongoose.Schema(
       type: String, // Corrected type
       required: true,
     },
-    isAdmin: {
-      type: Boolean,
-      required: true,
-      default: false,
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    refreshToken: {
+      type: String,
     },
   },
   { timestamps: true }
 );
+
+//Encrypting the password
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10)
+  next();
+})
+
+//Checking of password from user vs the encrypted password in db
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+//Method for generating access token
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+//method for creating refresh token
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
